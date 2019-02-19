19a1d108a57e64179de4df0c60477ddc2eecce89
kakerlakenpopo <45581064+kakerlakenpopo@users.noreply.github.com>
2019-02-19 18:21:08 +0100

    responsive adjust

--- script.js
+++ script.js
@@ -13,14 +13,38 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+var item = document.getElementsByClassName("item");
+  var img = item.getElementsByTagName('img')[0];
+
+  document.getElementsByClassName("item").style.display = 'none';
+  
+function enlarge() {
+  
+
+
+  if (event.target.style.width == '100%') {
+
+    event.target.style.width = '200%';
+    event.target.style.position = "relative";
+    event.target.style.zIndex = "1";
+    
+  } else {
+    event.target.style.width = '100%';
+    event.target.style.position = "relative";
+    event.target.style.zIndex = "0";
+  }
+
 }
